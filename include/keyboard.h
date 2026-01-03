cbc888aa281bce1c088d71735adf1861a6199144
HugeBrain16 <hugebrain16@gmail.com>
2026-01-03 14:27:52 +0700

    handle shift

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,1 +1,9 @@
+#define KEY_ALT 0x38
+#define KEY_CTRL 0x1D
+#define KEY_LSHIFT 0x2A
+#define KEY_RSHIFT 0x36
+#define KEY_RELEASE 0x80
+
+int keyboard_shift;
+
 void keyboard_handle();
--- src/keyboard.c
+++ src/keyboard.c
@@ -1,47 +1,76 @@
+#include "keyboard.h"
 #include "io.h"
 #include "ps2.h"
 #include "command.h"
 #include "terminal.h"
 #include "color.h"
 
-static char scancode_to_char(uint8_t scancode) {
-    static const char ascii[] = {
-         0, 0, '1', '2', '3', '4', '5', '6',
-        '7', '8', '9', '0', '-', '=', '\b', '\t',
-        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i',
-        'o', 'p', '[', ']', '\n', 0,  'a', 's',
-        'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-        '\'', '`', 0,  '\\', 'z', 'x', 'c', 'v',
-        'b', 'n', 'm', ',', '.', '/', 0,  '*',
-        0, ' '
-    };
+int keyboard_shift = 0;
 
+static const char ascii[] = {
+    0, 0, '1', '2', '3', '4', '5', '6',
+    '7', '8', '9', '0', '-', '=', '\b', '\t',
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i',
+    'o', 'p', '[', ']', '\n', 0,  'a', 's',
+    'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+    '\'', '`', 0,  '\\', 'z', 'x', 'c', 'v',
+    'b', 'n', 'm', ',', '.', '/', 0,  '*',
+    0, ' '
+};
+
+static const char ascii_shift[] = {
+    0, 0, '!', '@', '#', '$', '%', '^',
+    '&', '*', '(', ')', '_', '+', '\b', '\t',
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
+    'O', 'P', '{', '}', '\n', 0,  'A', 'S',
+    'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',
+    '"', '~', 0,  '|',  'Z', 'X', 'C', 'V',
+    'B', 'N', 'M', '<', '>', '?', 0,  '*',
+    0, ' '
+};
+
+static char scancode_to_char(uint8_t scancode) {
     if (sizeof(ascii) > scancode)
-        return ascii[scancode];
+        return keyboard_shift ? ascii_shift[scancode] : ascii[scancode];
 
     return 0;
 }
 
+static void keyboard_handle_type(uint8_t scancode) {
+    if (scancode & KEY_RELEASE) {
+        uint8_t key = scancode & 0x7F;
+
+        if (key == KEY_LSHIFT || key == KEY_RSHIFT)
+            keyboard_shift = 0;
+    }
+
+    if (scancode == KEY_LSHIFT || scancode == KEY_RSHIFT)
+        keyboard_shift = 1;
+
+    char c = scancode_to_char(scancode);
+    if (!c)
+        return;
+
+    char s[2] = { c, '\0' };
+    term_write(s, COLOR_WHITE, COLOR_BLACK);
+
+    if (c != '\b' && c != '\n') {
+        term_input[term_input_cursor++] = c;
+        term_input[term_input_cursor] = '\0';
+    }
+
+    if (c == '\n') {
+        command_handle(term_input);
+        term_input_cursor = 0;
+        term_input[0] = '\0';
+
+        term_write("\n> ", COLOR_WHITE, COLOR_BLACK);
+    }
+}
+
 void keyboard_handle() {
     uint8_t scancode = inb(PS2_DATA_PORT);
-    char c = scancode_to_char(scancode);
-    
-    if (c > 0 && scancode != 0x80 && term_mode == TERM_MODE_TYPE) {
-        char s[2] = {c, '\0'};
-        term_write(s, COLOR_WHITE, COLOR_BLACK);
-        
-        if (c != '\b' && c != '\n') {
-            term_input[term_input_cursor++] = c;
-            term_input[term_input_cursor] = '\0';
-        }
 
-        if (c == '\n') {
-            command_handle(term_input);
-            term_input_cursor = 0;
-            term_input[0] = '\0';
-
-            if (term_mode == TERM_MODE_TYPE)
-                term_write("\n> ", COLOR_WHITE, COLOR_BLACK);
-        }
-    }
+    if (term_mode == TERM_MODE_TYPE)
+        keyboard_handle_type(scancode);
 }
