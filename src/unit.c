a99d0ea1a68d06909365e72fd668697b9d4a3440
HugeBrain16 <hugebrain16@gmail.com>
2026-03-23 02:21:21 +0700

    unit with fraction

--- src/unit.c
+++ src/unit.c
@@ -1,12 +1,18 @@
 #include "string.h"
 
 void unit_get_size(uint32_t size, char *buffer) {
-    if ((size >> 30) > 0)
-        return strfmt(buffer, "%d GB", size >> 30);
-    else if ((size >> 20) > 0)
-        return strfmt(buffer, "%d MB", size >> 20);
-    else if ((size >> 10) > 0)
-        return strfmt(buffer, "%d KB", size >> 10);
-
+    if ((size >> 30) > 0) {
+        uint32_t whole = size >> 30;
+        uint32_t frac  = ((size - (whole << 30)) * 100) >> 30;
+        return strfmt(buffer, "%d.%d GB", whole, frac);
+    } else if ((size >> 20) > 0) {
+        uint32_t whole = size >> 20;
+        uint32_t frac  = ((size - (whole << 20)) * 100) >> 20;
+        return strfmt(buffer, "%d.%d MB", whole, frac);
+    } else if ((size >> 10) > 0) {
+        uint32_t whole = size >> 10;
+        uint32_t frac  = ((size - (whole << 10)) * 100) >> 10;
+        return strfmt(buffer, "%d.%d KB", whole, frac);
+    }
     return strfmt(buffer, "%d B", size);
 }
